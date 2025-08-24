eb07727f1f0aec5d7f978f7936341f94d9d509a5
Emre Karabey <emrekarabey229@gmail.com>
2025-08-24 22:54:14 +0300

    Data Access Layer Katmanı Tamamlanıldı

---
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+
+namespace DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGeneric<T> where T : class
+    {
+        public void Add(T t)
+        {
+            using var c = new Context();
+
+            c.Set<T>().Add(t);
+
+            c.SaveChanges();
+        }
+
+        public void Delete(T t)
+        {
+            using var c = new Context();
+
+            c.Set<T>().Remove(t);
+
+            c.SaveChanges();
+        }
+
+        public List<T> list(T t)
+        {
+           using var c = new Context();
+
+            return c.Set<T>().ToList();
+        }
+
+        public void Update(T t)
+        {
+            using var c = new Context();
+
+            c.Set<T>().Update(t);
+
+            c.SaveChanges();
+        }
+    }
+}
--- DataAccessLayer/obj/Debug/net9.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net9.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82185a08a22998f00fe1f8c108cfde2d68085b38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d86898025dba7ed4a9c4516d61ebb9909a3f6d4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
