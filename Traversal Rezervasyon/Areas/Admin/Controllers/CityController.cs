d7dd5f0229a26b5db1a0e247c2a276f2c48dfe63
Emre Karabey <emrekarabey229@gmail.com>
2025-09-26 01:07:36 +0300

    Ajax Silme ve Güncelleme işlemleri getirildi

--- Traversal Rezervasyon/Areas/Admin/Controllers/CityController.cs
+++ Traversal Rezervasyon/Areas/Admin/Controllers/CityController.cs
@@ -5,6 +5,7 @@ using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Traversal_Rezervasyon.Areas.Admin.Models;
+using Destination = EntityLayer.Concrete.Destination;
 
 namespace Traversal_Rezervasyon.Areas.Admin.Controllers
 {
@@ -37,12 +38,57 @@ namespace Traversal_Rezervasyon.Areas.Admin.Controllers
             return Json(destination);
         }
 
-        public IActionResult GetByCityId(int id)
+        [HttpGet]
+        public IActionResult GetByCityId(int DestinationId)
         {
-            var values = _ıdestinationservices.GetById(id);
+            var values = _ıdestinationservices.GetById(DestinationId);
+            if (values == null)
+            {
 
-            return Json(values);
+            }
+            else
+            {
+                return Json(values);
+            }
 
+            return View();
+
+        }
+
+        public IActionResult DeleteCity(int id)
+        {
+          var values =  _ıdestinationservices.GetById(id);
+
+            _ıdestinationservices.Delete(values);
+
+            return NoContent();
+        }
+        [HttpPost]
+        public IActionResult UpdateCity(Destination destinations)
+        {
+            var gstr = _ıdestinationservices.GetById(destinations.DestinationID);
+
+            destinations.DestinationID = gstr.DestinationID;
+    
+            destinations.Quotation = gstr.Quotation;
+            destinations.Reservations = gstr.Reservations;
+            destinations.Comments = gstr.Comments;
+            destinations.Status = gstr.Status;
+            destinations.CoverImage = gstr.CoverImage;
+            destinations.Image = gstr.Image;
+            destinations.Image2 = gstr.Image2;
+            destinations.Details1 = gstr.Details1;
+            destinations.Details2 = gstr.Details2;
+            destinations.Details3 = gstr.Details3;
+            destinations.Title1 = gstr.Title1;
+            destinations.Title2 = gstr.Title2;
+            destinations.MainTitle = gstr.MainTitle;
+
+
+
+            _ıdestinationservices.Update(destinations);
+
+            return Json(destinations);
         }
 
 
